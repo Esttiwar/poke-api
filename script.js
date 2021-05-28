44bc9f005ec93bf9687dcda4a3dd45b35d585c78
estarossa <jestiwar.correa@gmail.com>
2021-05-28 00:05:43 -0500

    update images

--- script.js
+++ script.js
@@ -146,7 +146,7 @@ async function getPokemon(i) {
 
         const $imgAccessPoke = document.createElement("img");
         $imgAccessPoke.className = "img-access-poke";
-        $imgAccessPoke.setAttribute("src","/images/background.png")
+        $imgAccessPoke.setAttribute("src","https://i.postimg.cc/hv6s9Spk/background.png")
         
 
 
